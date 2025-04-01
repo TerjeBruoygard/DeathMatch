a9df15f678a107a2adb1dea7982643d3f3cf188b
Ben Potter <benpotterlikesmath@gmail.com>
2025-04-01 01:23:39 -0400

    remove unneeded server settings properties

--- DeathMatch/Scripts/3_Game/DmServerSettings.c
+++ DeathMatch/Scripts/3_Game/DmServerSettings.c
@@ -1,43 +1,43 @@
 class DM_ServerSettings
 {
-	ref array<ref DM_AreaTrail> m_trails = new array<ref DM_AreaTrail>;
-	float m_expandStep = 10;
+	// ref array<ref DM_AreaTrail> m_trails = new array<ref DM_AreaTrail>;
+	// float m_expandStep = 10;
 	float m_healthRegen = 1.0;
-	float m_healthOutOfZoneDmg = 0.1;
+	// float m_healthOutOfZoneDmg = 0.1;
 	float m_healthRegenTimeoutOnHit = 10;
 	int m_startMoney = 100;
 	int m_moneyPerKill = 10;
-	float m_expIncPerKill = 0.2;
-	float m_expDecPerDeath = 0.1;
-	float m_areaMoveSpeed = 0.2;
-	int m_databaseSaveTime = 600;
-	int m_databaseWipePlayerTimeout = 1209600; // 14 days
-	int m_leaderBoardTopSize = 20;
+	// float m_expIncPerKill = 0.2;
+	// float m_expDecPerDeath = 0.1;
+	// float m_areaMoveSpeed = 0.2;
+	// int m_databaseSaveTime = 600;
+	// int m_databaseWipePlayerTimeout = 1209600; // 14 days
+	// int m_leaderBoardTopSize = 20;
 	int m_lockWorldTime = 1;
 	float m_fastRespawnTimeout = 3;
 	int m_clearDroppedItemsOnGround = 1;
 	
 	// will not be called
-	void InitDefaultTrails()
-	{
-		ref DM_AreaTrail trail = new DM_AreaTrail;
-		trail.m_points.Insert("5281.88 0 9798.75");
-		trail.m_points.Insert("5048.44 0 9757.50");
-		trail.m_points.Insert("4755.00 0 10384.69");
-		trail.m_points.Insert("4504.69 0 10818.75");
-		trail.m_points.Insert("4153.13 0 11025.00");
-		trail.m_points.Insert("4020.94 0 11762.81");
-		trail.m_points.Insert("4153.13 0 11025.00");
-		trail.m_points.Insert("4504.69 0 10818.75");
-		trail.m_points.Insert("4755.00 0 10384.69");
-		trail.m_points.Insert("5048.44 0 9757.50");
-		m_trails.Insert(trail);
-	}
+	// void InitDefaultTrails()
+	// {
+	// 	ref DM_AreaTrail trail = new DM_AreaTrail;
+	// 	trail.m_points.Insert("5281.88 0 9798.75");
+	// 	trail.m_points.Insert("5048.44 0 9757.50");
+	// 	trail.m_points.Insert("4755.00 0 10384.69");
+	// 	trail.m_points.Insert("4504.69 0 10818.75");
+	// 	trail.m_points.Insert("4153.13 0 11025.00");
+	// 	trail.m_points.Insert("4020.94 0 11762.81");
+	// 	trail.m_points.Insert("4153.13 0 11025.00");
+	// 	trail.m_points.Insert("4504.69 0 10818.75");
+	// 	trail.m_points.Insert("4755.00 0 10384.69");
+	// 	trail.m_points.Insert("5048.44 0 9757.50");
+	// 	m_trails.Insert(trail);
+	// }
 };
 
-class DM_AreaTrail
-{
-	ref array<vector> m_points = new array<vector>;
-	float m_minRadius = 120;
-	float m_maxRadius = 1500;
-};
+// class DM_AreaTrail
+// {
+// 	ref array<vector> m_points = new array<vector>;
+// 	float m_minRadius = 120;
+// 	float m_maxRadius = 1500;
+// };
